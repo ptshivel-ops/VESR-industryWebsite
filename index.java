3f5e850658e42114a692c2fdfc447d86440c935c
ptshivel-ops <ptshivel@gmail.com>
2025-10-27 13:50:56 -0400

    Add files via upload

---
+++ index.java
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Values</title>
+    <style>
+        h1 {color: blanchedalmond; /Dark Grey}
+        header {background-color: grey;}
+        h2 {font-family: 'Arial', sans-serif; 
+  font-size: 24px;                 
+  font-weight: bold;               
+  font-style: italic;            
+  color: #333;                   
+  text-transform: uppercase;}
+  h2 {color: cornflowerblue;}
+
+
+        </style>
+        </head>
+        <body>
+            <header>
+                <h1>My Personal Values</h1>
+            </nav>
+            <a href="index.html">Home</a>
+            <a href="cyber.html">My Values</a>
+        </nav>
+        </header>
+        <main>
+            <img src="https://c8.alamy.com/comp/2kkxac1/4k-personal-values-animated-tag-word-cloud-text-design-animation-seamless-loop-2kkxac1.jpg" alt="Personal Values Word Cloud" height="300" width="400">
+            <h2>My Top Four Personal Values</h2>
+            <div>
+                <h3>Hard Work</h3>
+                <p>Why is this important to you?</p>
+                <p>Hard work is important to me because it is important to try your hardest in everything you do, because you never know what results may occur.</p>
+                <p>How did you develop this value?</p>
+                <p> I developed this value from playing soccer and feeling guilty if I didn't try hard enough and my team suffered because of it.</p>
+                </div>
+                <div>
+                    <h3>Accountability</h3>
+                    <p>Why is this important to you?</p>
+                    </p>Accountability is important to me because people need to be able to trust you in every circumstance.<p>
+                    <p>How did you develop this value?</p>
+                    </p>I developed this value from my parents holding me accountable in school.<p>
+                </div>
+                <div>
+                    <h3>Integrity</h3>
+                    <p>Why is this important to you?</p>
+                    <p>Integrity is important to me because you should always be doing what you are supposed to be doing no matter if or who is watching you.</p>
+                     <p>How did you develop this value?</p>
+                     <p>I develpoped this value in elementary school when my teachers would leave the classroom and expect us to show integrity</p>
+                     <div>
+                    <h4>Discipline</h4>
+                    <p>Why is this important to you?</p>
+                    <p>It is important to stay disciplined because it is one of the only ways to prove to yourself that you can grow and improve.</p>
+                     <p>How did you develop this value?</p>
+                     <p>I developed this value while doing homework, practicing soccer, and working out.</p>
+                     <div>
+            </main>
+            <footer> 
+                <p>&copy; 2025 My Personal Values</p>
+                </footer>
+                </body>
+</html>
